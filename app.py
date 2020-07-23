a85e13ee230d6ab9c78bf8e9a69d004d022eb4be
Gamygams1234 <gamyburgos@gmail.com>
2020-07-23 16:08:33 -0700

    Making logout to end the session

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, g, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user, login_required
 from flask_bcrypt import check_password_hash
 import forms
 
@@ -71,6 +71,14 @@ def login():
             else:
                 flash("Your email or password does not exist.")
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out! come back soon!", "success")
+    return redirect(url_for('index'))
 
 
 @app.route('/')
